69ca2de73f5e12e3bf6db60b590878747b9b7516
XORs33r <bugislife@proton.me>
2024-06-29 20:56:52 +0800

    feat: hack for scrollable element

--- src/main.tsx
+++ src/main.tsx
@@ -6,8 +6,40 @@ import "./index.css";
 
 import WebApp from "@twa-dev/sdk";
 
+// App in full screenmode
 WebApp.ready();
 WebApp.expand();
+
+// Avoid close on scrolling down
+const overflow = 100;
+document.body.style.overflowY = "hidden";
+document.body.style.marginTop = `${overflow}px`;
+document.body.style.height = window.innerHeight + overflow + "px";
+document.body.style.paddingBottom = `${overflow}px`;
+window.scrollTo(0, overflow);
+
+let ts: number | undefined;
+const onTouchStart = (e: TouchEvent) => {
+  ts = e.touches[0].clientY;
+};
+const scrollableEl = document.querySelector(".scrollable");
+const onTouchMove = (e: TouchEvent) => {
+  if (scrollableEl) {
+    const scroll = scrollableEl.scrollTop;
+    const te = e.changedTouches[0].clientY;
+    if (scroll <= 0 && ts! < te) {
+      e.preventDefault();
+    }
+  } else {
+    e.preventDefault();
+  }
+};
+document.documentElement.addEventListener("touchstart", onTouchStart, {
+  passive: false,
+});
+document.documentElement.addEventListener("touchmove", onTouchMove, {
+  passive: false,
+});
 
 // this manifest is used temporarily for development purposes
 const manifestUrl =
