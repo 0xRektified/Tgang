305fe1390461c01a5386fe8990327ebba89d3db1
Alko89 <alefer89@gmail.com>
2024-07-10 22:05:42 +0200

    Social page

--- src/components/social/Social.tsx
+++ src/components/social/Social.tsx
@@ -1,15 +1,25 @@
 import React, { useState, useEffect } from "react";
-import {
-  FlexBoxRow,
-} from "../styled/shopStyled";
-import { Card } from "../styled/globalStyled";
 import WebApp from "@twa-dev/sdk";
+import { PiCopySimple } from "react-icons/pi";
+import styled from "styled-components";
 
 interface SocialProps {
   referralToken: string;
   referredUsers: string[];
   activeTab: string;
 }
+
+const SocialContainer = styled.div`
+background-color: #1c1c1e;
+min-height: 80vh;
+display: flex;
+flex-direction: column;
+align-items: center;
+padding: 1rem;
+width: 100%;
+border-radius: 0.375rem;
+box-shadow: 0 4px 6px rgba(0, 0, 0, 0.5);
+`;
 
 export const Social: React.FC<SocialProps> = ({
   referralToken,
@@ -32,29 +42,50 @@ export const Social: React.FC<SocialProps> = ({
   };
 
   return (
-    <FlexBoxRow>
-      <h1>Referral Link</h1>
-      <Card onClick={handleRefForward}>
-        Share
-      </Card>
-      <Card onClick={handleRefClick}>
-        {referralToken}
-      </Card>
-      <table>
-        <thead>
-          <tr>
-            <th>Referred Users</th>
-          </tr>
-        </thead>
-        <tbody>
-          {referredUsers.map((user) => (
-            <tr key={user}>
-              <td>{user}</td>
-            </tr>
-          ))}
-        </tbody>
-      </table>
-    </FlexBoxRow>
+    <SocialContainer>
+      <div className="flex w-full flex-col" >
+        <div className="card bg-base-300 grid place-items-center">
+          <div className="flex w-full place-items-center">
+            <div className="card bg-base-300 rounded-box grid h-20 place-items-center">
+                <button className="btn btn-success" onClick={handleRefForward}>
+                  Invite Friends
+                </button>
+            </div>
+            <div className="divider-horizontal"></div>
+            <div className="card bg-base-300 rounded-box grid h-20 place-items-center">
+                <button className="btn btn-success" onClick={handleRefClick}>
+                  <PiCopySimple />
+                </button>
+            </div>
+          </div>
+        </div>
+        <div className="card bg-base-300 grid place-items-center">
+          <div className="stats shadow">
+            <div className="stat place-items-center">
+              <div className="stat-desc">Invited users</div>
+              <div className="stat-value">{referredUsers.length}</div>
+              <div className="stat-title">Airdrop to be announced</div>
+            </div>
+          </div>
+          <div className="overflow-x-auto">
+            <table className="table table-lg">
+              <thead>
+                <tr>
+                  <th>Referred Users</th>
+                </tr>
+              </thead>
+              <tbody>
+                {referredUsers.map((user) => (
+                  <tr key={user}>
+                    <td>{user}</td>
+                  </tr>
+                ))}
+              </tbody>
+            </table>
+          </div>
+        </div>
+      </div>
+    </SocialContainer>
   );
 };
 
