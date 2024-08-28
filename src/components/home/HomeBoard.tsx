9425cc1bfe8186c52ebc47cee5f595a0eef32caf
Alko89 <alefer89@gmail.com>
2024-08-28 17:53:58 +0200

    fix: premium invite rewards

---
+++ src/components/home/HomeBoard.tsx
@@ -0,0 +1,163 @@
+import React from "react";
+import styled from "styled-components";
+import { LastTransaction } from "./LastTransaction";
+import { Transaction } from "./utils/types";
+
+const CustomerEmojiContainer = styled.div`
+  width: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  position: relative;
+`;
+
+const FlexBoxColNoGap = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 0;
+  align-items: center;
+`;
+
+const CustomerEmoji = styled.span`
+  font-size: 1.3rem;
+  margin: 0rem;
+  &.customer-emoji {
+    position: relative;
+  }
+`;
+
+const CustomerListContainer = styled.div`
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: center;
+  max-width: 100%;
+  scrollbar-width: thin;
+  scrollbar-color: #4a5568 #2d3748;
+
+  &::-webkit-scrollbar {
+    height: 8px;
+  }
+
+  &::-webkit-scrollbar-track {
+    background: #2d3748;
+  }
+
+  &::-webkit-scrollbar-thumb {
+    background-color: #4a5568;
+    border-radius: 4px;
+  }
+`;
+
+const emojiList = [
+  "👶",
+  "🧒",
+  "👦",
+  "👧",
+  "🧑",
+  "👱",
+  "👨",
+  "🧔",
+  "👶🏿",
+  "🧒🏿",
+  "👦🏿",
+  "👧🏿",
+  "🧑🏿",
+  "👱🏿",
+  "👨🏿",
+  "🧔🏿",
+  "👶🏻",
+  "🧒🏻",
+  "👦🏻",
+  "👧🏻",
+  "🧑🏻",
+  "👱🏻",
+  "👨🏻",
+  "🧔🏻",
+  "👶🏽",
+  "🧒🏽",
+  "👦🏽",
+  "👧🏽",
+  "🧑🏽",
+  "👱🏽",
+  "👨🏽",
+  "🧔🏽",
+  "👶🏾",
+  "🧒🏾",
+  "👦🏾",
+  "👧🏾",
+  "🧑🏾",
+  "👱🏾",
+  "👨🏾",
+  "🧔🏾",
+];
+
+const WaitingCustomers = styled.p`
+  color: #cbd5e0;
+  font-size: 0.8rem;
+  text-align: right;
+`;
+
+export const FlexBoxRow = styled.div`
+  display: flex;
+  flex-direction: row;
+  gap: 10px;
+  align-items: center;
+  margin-top: 2em;
+  background-color: rgba(0, 0, 0, 0.8);
+  padding: 0.1em;
+  border-radius: 0.5rem;
+  z-index: 2;
+`;
+const CustomerAmount = styled.p`
+  color: #cbd5e0;
+  font-size: 0.8rem;
+  text-align: right;
+  margin-top: 0.5em;
+`;
+interface CustomersBoardProps {
+  customer: string;
+  customerAmount: number;
+  transaction: Transaction | null;
+  animatingEmojis: { emoji: string; id: number; offset: string }[];
+}
+
+export const getRandomEmoji = () => {
+  return emojiList[Math.floor(Math.random() * emojiList.length)];
+};
+
+export const HomeBoard: React.FC<CustomersBoardProps> = ({
+  customer,
+  customerAmount,
+  transaction,
+  animatingEmojis,
+}) => {
+  return (
+    <FlexBoxRow className="w-full justify-center">
+      <FlexBoxColNoGap className="">
+        <WaitingCustomers>Customers</WaitingCustomers>
+        <CustomerEmojiContainer>
+          <CustomerListContainer>
+            <CustomerAmount>{customerAmount}</CustomerAmount>
+            {customerAmount > 0 ? (
+              <CustomerEmoji className="customer-emoji">
+                {customer}
+              </CustomerEmoji>
+            ) : (
+              <CustomerEmoji>🫥</CustomerEmoji>
+            )}
+          </CustomerListContainer>
+        </CustomerEmojiContainer>
+        {animatingEmojis.map(({ emoji, id, offset }) => (
+          <div
+            key={`${id}${offset}`}
+            className="absolute bottom-20 left-1/2 transform -translate-x-1/2 text-2xl text-white animate-move-up-random-x "
+            style={{ "--random-offset": offset } as React.CSSProperties}
+          >
+            {emoji}
+          </div>
+        ))}
+        <LastTransaction transaction={transaction} />
+      </FlexBoxColNoGap>
+    </FlexBoxRow>
+  );
+};
---
+++ src/components/home/styles/shipping.css.tsx
@@ -0,0 +1,249 @@
+import styled from "styled-components";
+
+export const CardContainer = styled.div<{ locked?: boolean }>`
+  display: flex;
+  flex-direction: column;
+  background-color: rgba(128, 128, 128, 0.15);
+  border-radius: 0.8rem;
+  padding: 1rem;
+  box-shadow: 0 0.4rem 0.6rem rgba(0, 0, 0, 0.1);
+  color: #e4e4e7;
+  margin-bottom: 1rem;
+  align-items: center;
+`;
+
+export const CardHeader = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+`;
+
+export const CardContent = styled.div`
+  margin-top: 1rem;
+`;
+
+export const CardTitle = styled.h4`
+  font-size: 1.2rem;
+  font-weight: bold;
+`;
+
+export const CardDescription = styled.p`
+  font-size: 0.9rem;
+  margin: 0.5rem 0;
+`;
+
+export const CardImage = styled.img`
+  width: 5rem;
+  height: 5rem;
+  border-radius: 0.8rem;
+`;
+
+export const CardDetails = styled.div`
+  display: flex;
+  justify-content: space-between;
+  flex: 1;
+`;
+
+export const CardInfoColumn = styled.div`
+  display: flex;
+  flex-direction: column;
+`;
+
+export const CardRequirement = styled.p`
+  font-size: 0.8rem;
+  color: #ff6b6b;
+`;
+
+export const NeonButtonShipping = styled.button`
+  background-color: rgb(39 39 42) !important;
+  color: #e4e4e7;
+  border-radius: 8px;
+  padding: 0.5rem 1rem;
+  margin: 1rem 0rem;
+  border: 1px solid #eab308;
+  cursor: pointer;
+  font-weight: bold;
+  font-size: 0.9em;
+  text-transform: uppercase;
+  letter-spacing: 0.05em;
+  display: flex;
+  align-items: center;
+  gap: 0.5rem;
+  transition: background-color 0.3s ease, transform 0.1s ease;
+
+  &:hover {
+    background-color: rgb(24 24 27);
+    animation: glow 1.5s infinite alternate, pulse 2s infinite;
+  }
+
+  &:active {
+    animation: glow 1.5s infinite alternate, pulse 2s infinite;
+  }
+
+  &.disabled {
+    background-color: rgb(99 99 99) !important;
+    border: none;
+  }
+`;
+
+export const NeonButtonShippingLocked = styled.button`
+  background-color: rgb(39 39 42) !important;
+  color: #e4e4e7;
+  border-radius: 8px;
+  padding: 0.5rem 1rem;
+  margin: 1rem 0rem;
+  border: 1px solid #eab308;
+  cursor: pointer;
+  font-weight: bold;
+  font-size: 0.9em;
+  text-transform: uppercase;
+  letter-spacing: 0.05em;
+  display: flex;
+  align-items: center;
+  gap: 0.5rem;
+  transition: background-color 0.3s ease, transform 0.1s ease;
+
+  &:hover {
+    background-color: rgb(24 24 27);
+    animation: glow 1.5s infinite alternate, pulse 2s infinite;
+  }
+
+  &:active {
+    animation: glow 1.5s infinite alternate, pulse 2s infinite;
+  }
+
+  &.disabled {
+    background-color: rgb(99 99 99) !important;
+    border: none;
+  }
+`;
+
+export const Button = styled.button`
+  background-color: rgb(39 39 42);
+  color: #e4e4e7;
+  border-radius: 0.8rem;
+  padding: 0.5rem 1rem;
+  border: 0.2rem solid;
+  cursor: pointer;
+  font-weight: bold;
+  font-size: 0.9rem;
+  text-transform: uppercase;
+  letter-spacing: 0.05em;
+  transition: background-color 0.3s ease;
+
+  &:hover {
+    background-color: rgb(24 24 27);
+  }
+
+  &:active {
+    transform: scale(0.95);
+  }
+`;
+
+export const ShippingCardsContainer = styled.div`
+  display: grid;
+  grid-template-columns: repeat(2, 1fr);
+  gap: 1rem;
+`;
+
+export const ModalContainer = styled.div`
+  position: fixed;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  width: 90%;
+  max-width: 400px;
+  background: #1a1a1a;
+  padding: 2rem;
+  border-radius: 1rem;
+  box-shadow: 0 0.5rem 1rem rgba(0, 0, 0, 0.5);
+  z-index: 1000;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`;
+
+export const CloseButton = styled.button`
+  position: absolute;
+  top: 1.4rem;
+  right: 1rem;
+  background: none;
+  border: none;
+  color: #e4e4e7;
+  font-size: 1.5rem;
+  cursor: pointer;
+
+  &:hover {
+    color: #ff0000;
+  }
+`;
+
+export const ScrollableTableContainer = styled.div`
+  flex-grow: 1;
+  overflow-y: auto;
+  scrollbar-width: thin;
+  scrollbar-color: #4a5568 #2d3748;
+  background-color: #1a202c;
+  width: 100%;
+  max-height: 60vh; /* Adjust this value as needed */
+
+  &::-webkit-scrollbar {
+    width: 8px;
+  }
+
+  &::-webkit-scrollbar-track {
+    background: #2d3748;
+  }
+
+  &::-webkit-scrollbar-thumb {
+    background-color: #4a5568;
+    border-radius: 4px;
+  }
+`;
+
+export const ClickableText = styled.div`
+  color: #007bff;
+  font-size: 1rem;
+  text-decoration: underline;
+  cursor: pointer;
+  text-align: center;
+  margin: 1rem 0;
+
+  &:hover {
+    color: #0056b3;
+  }
+`;
+
+export const SiteTitleTedex = styled.h1`
+  font-family: "Futura", sans-serif;
+  font-size: 1.5rem;
+  font-weight: bold;
+  text-align: center;
+  color: #ffffff;
+  margin: 0;
+  padding: 0;
+
+  .te {
+    color: #4d148c; /* FedEx Purple for "Te" */
+  }
+
+  .dex {
+    color: #ff6600; /* FedEx Orange for "dex" */
+  }
+`;
+export const SiteDescription = styled.p`
+  font-size: 0.8rem;
+  line-height: 1.5;
+  color: #cbd5e0;
+`;
+
+export const StyledLink = styled.p`
+  margin-top: 0.5em;
+  font-size: 0.8rem;
+  color: #1e90ff;
+  text-shadow: "0 0 8px #1e90ff";
+  cursor: pointer;
+  text-decoration: none;
+  font-weight: bold;
+  transition: color 0.3s ease, text-decoration 0.3s ease;
+`;
