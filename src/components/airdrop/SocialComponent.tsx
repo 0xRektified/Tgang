325b53fd7da308e5cc26e95676770aedf2bbb2d8
XORs33r <bugislife@proton.me>
2024-09-06 00:18:38 +0700

    feat: update socials css

--- src/components/airdrop/SocialComponent.tsx
+++ src/components/airdrop/SocialComponent.tsx
@@ -17,11 +17,49 @@ import { useVerifySocial } from "../../hooks/useVerifySocial";
 import { ApiToast } from "../ApiToast";
 import { useJoinSocial } from "../../hooks/useJoinSocial";
 
-export const CardImageSocial = styled(CardImage)`
-  width: 50px;
-  height: 50px;
+const ColoredText = styled.span`
+  font-size: 1rem;
+  font-weight: bold;
+  text-shadow: 0 0 2px currentColor;
+  transition: text-shadow 0.3s ease;
+
+  &.cash {
+    color: #2e9dff;
+  }
+  &.rep {
+    color: #2e9dff;
+  }
+`;
+
+const CardImageContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`;
+
+const RewardText = styled.div`
+  margin-top: 0.25rem;
+  text-align: center;
+  display: flex;
+  flex-direction: column;
+`;
+
+export const CardImageSocial = styled(CardImage)<{ $title: string }>`
+  width: ${({ $title }) =>
+    $title === "Cartel X"
+      ? "40px"
+      : $title === "Cartel Youtube"
+      ? "60px"
+      : "50px"};
+  height: ${({ $title }) =>
+    $title === "Cartel X"
+      ? "40px"
+      : $title === "Cartel Youtube"
+      ? "60px"
+      : "50px"};
   border-radius: 0.5rem;
 `;
+
 interface SocialComponentProps {
   socials: Record<SocialChannel, SocialData>;
   userInfo: IUserInfo;
@@ -88,9 +126,9 @@ const SocialComponent: React.FC<SocialComponentProps> = ({
   };
 
   return (
-    <Card>
+    <Card style={{ marginBottom: "10em" }}>
       <StatDesc className="font-bold mb-4">
-        Join our socials to get $1000 and 100 rep each!
+        Follow our socials to get some Alpha
       </StatDesc>
       <div className="space-y-2">
         {Object.entries(socials).map(([channel, social]) => {
@@ -100,19 +138,26 @@ const SocialComponent: React.FC<SocialComponentProps> = ({
           const isMember = userSocial?.member || false;
 
           return (
-            <CardContainer style={{ padding: "0rem" }}>
+            <CardContainer style={{ padding: "0rem", marginBottom: "1rem" }}>
               <CardHeader>
-                <CardImageSocial
-                  src={social.image}
-                  alt={social.title}
-                  loading="lazy"
-                />
+                <CardImageContainer>
+                  <CardImageSocial
+                    src={social.image}
+                    alt={social.title}
+                    loading="lazy"
+                    $title={social.title}
+                  />
+
+                  <RewardText>
+                    <ColoredText className="cash">+1000$</ColoredText>
+                    <ColoredText className="rep">+100rp</ColoredText>
+                  </RewardText>
+                </CardImageContainer>
                 <CardDetails>
                   <CardInfoColumn>
                     <CardTitle>
                       {social.title} {!!isMember ? `✅` : ``}
                     </CardTitle>
-
                     <div className="grid grid-cols-2 gap-4 w-full pb-4">
                       {isMember ? (
                         <div className="card flex flex-col justify-center items-center text-center col-span-2">
