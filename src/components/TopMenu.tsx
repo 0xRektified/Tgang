e39870478435e1189852ed1b41cd9340ca0ece49
XORs33r <bugislife@proton.me>
2024-09-06 12:41:49 +0700

    feat: unify design and improve global apparence

---
+++ src/components/TopMenu.tsx
@@ -0,0 +1,94 @@
+import React, { useLayoutEffect, useRef, useEffect, useState } from "react";
+import { IUserInfo } from "./interfaces/user.interface";
+import rank from "/assets/rank.png";
+import {
+  TopMenuContainer,
+  Container,
+  RankIcon,
+  DigitalFont,
+  Username,
+  BalanceLabel,
+  BalanceAmount,
+  LevelInfo,
+  ProgressBar,
+} from "./styled/topmenu";
+import { FlexBoxCol, FlexBoxRow } from "./styled/globalStyled";
+import { calculateProgress, formatPrice } from "./utils/formater";
+
+// Add this function at the top of the file, outside the component
+const calculateFontSize = (username: string): string => {
+  if (username.length <= 10) return "1.2rem";
+  if (username.length <= 15) return "1.1rem";
+  if (username.length <= 20) return "1rem";
+  if (username.length <= 25) return "0.8rem";
+  return "0.7rem";
+};
+
+interface TopMenuProps {
+  userInfo: IUserInfo;
+}
+
+export const TopMenu: React.FC<TopMenuProps> = ({ userInfo }) => {
+  const [fontSize, setFontSize] = useState("1rem");
+  const usernameRef = useRef<HTMLSpanElement>(null);
+
+  useLayoutEffect(() => {
+    const scrollableEl = document.getElementById("mainView");
+    if (scrollableEl) {
+      requestAnimationFrame(() => {
+        scrollableEl.scrollIntoView({ behavior: "smooth" });
+      });
+    }
+  }, []);
+
+  useEffect(() => {
+    if (userInfo && userInfo.username) {
+      setFontSize(calculateFontSize(userInfo.username));
+    }
+  }, [userInfo]);
+
+  if (!userInfo) return null;
+
+  const { reputation, userLevel, username, cashAmount } = userInfo;
+  const { level, title, minReputation, maxReputation } = userLevel;
+  const progress = calculateProgress(reputation, minReputation, maxReputation);
+
+  return (
+    <TopMenuContainer id="mainView">
+      <Container>
+        <FlexBoxRow className="w-full justify-between items-center">
+          <FlexBoxCol>
+            <div className="flex items-center space-x-2">
+              <RankIcon src={rank} alt="Rank" />
+              <DigitalFont as={Username} ref={usernameRef} style={{ fontSize }}>
+                {username}
+              </DigitalFont>
+            </div>
+          </FlexBoxCol>
+          <FlexBoxCol>
+            <div className="flex justify-end space-x-2">
+              <DigitalFont as={BalanceLabel}>Cash</DigitalFont>
+              <DigitalFont as={BalanceAmount}>
+                {formatPrice(cashAmount, false)}
+              </DigitalFont>
+            </div>
+          </FlexBoxCol>
+        </FlexBoxRow>
+        <FlexBoxCol className="w-full">
+          <LevelInfo>
+            <span>
+              Lvl {level} {title}
+            </span>
+            <ProgressBar
+              className=" progress progress-warning w-40"
+              value={progress}
+              max="100"
+            ></ProgressBar>
+          </LevelInfo>
+        </FlexBoxCol>
+      </Container>
+    </TopMenuContainer>
+  );
+};
+
+export default TopMenu;
