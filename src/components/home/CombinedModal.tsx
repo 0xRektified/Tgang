db29362f190aa88f87ef5b398a5760c7042d12bd
XORs33r <bugislife@proton.me>
2024-09-10 15:06:24 +0700

    fix: leaderBoard asset

---
+++ src/components/home/CombinedModal.tsx
@@ -0,0 +1,201 @@
+import React, { useState, useEffect } from "react";
+import WebApp from "@twa-dev/sdk";
+import {
+  CloseButton,
+  FixedOverlay,
+  ModalContainer,
+  Notch,
+  RoundButton,
+  TabContainer,
+  Tab,
+} from "./styles/supplier.css";
+import { tabMapping } from "../interfaces/general.interface";
+import useBuyProduct from "../../hooks/useBuyProduct";
+import useShipProduct from "../../hooks/useShipProduct";
+import { IUserInfo, Product } from "../interfaces/user.interface";
+import { IMarketInfo, MarketProduct } from "../interfaces/market.interface";
+import { EProduct } from "../interfaces/product.interface";
+import { TilkRoadModal } from "./TilkRoadModal";
+import { TedexModal } from "./TedexModal";
+import {
+  EShippingMethod,
+  IShippingMethod,
+} from "../interfaces/shipping.interface";
+import { useTutorial } from "../../hooks/useTutorial";
+
+interface ModalProps {
+  userInfo: IUserInfo;
+  marketInfo: IMarketInfo | undefined;
+  isOpen: boolean;
+  onClose: () => void;
+  onUnlockClick: (tab?: string | undefined) => void;
+  setUserInfo: React.Dispatch<React.SetStateAction<IUserInfo>>;
+  shippingMethods: Record<EShippingMethod, IShippingMethod> | undefined;
+  tutorial: ReturnType<typeof useTutorial>;
+  handleTutorialComplete: () => void;
+}
+
+export const CombinedModal: React.FC<ModalProps> = ({
+  userInfo,
+  marketInfo,
+  isOpen,
+  onClose,
+  onUnlockClick,
+  setUserInfo,
+  shippingMethods,
+  tutorial,
+  handleTutorialComplete,
+}) => {
+  const [activeTab, setActiveTab] = useState("Tilk Road");
+  const { buyProduct, loading, error } = useBuyProduct();
+  const { shipProduct } = useShipProduct();
+  const [selectedProduct, setSelectedProduct] = useState<
+    Product | MarketProduct | null
+  >(null);
+  const [quantity, setQuantity] = useState<number>(1);
+  const [showToast, setShowToast] = useState<boolean>(false);
+  const [totalCost, setTotalCost] = useState<number>(0);
+  const [remainingCash, setRemainingCash] = useState<number>(
+    userInfo.cashAmount,
+  );
+  const [purchaseAmount, setPurchaseAmount] = useState(0);
+
+  useEffect(() => {
+    if (selectedProduct && "discountPrice" in selectedProduct) {
+      const newTotalCost = selectedProduct.discountPrice * quantity;
+      setTotalCost(newTotalCost);
+    } else {
+      setTotalCost(0);
+    }
+    setRemainingCash(userInfo.cashAmount);
+  }, [selectedProduct, quantity, userInfo.cashAmount]);
+
+  useEffect(() => {
+    if (isOpen && tutorial.tutorialStep === 2) {
+      setActiveTab("Tilk Road");
+    }
+  }, [isOpen, tutorial.tutorialStep]);
+
+  const handleBuy = async () => {
+    if (totalCost > userInfo.cashAmount) {
+      setShowToast(true);
+      setTimeout(() => setShowToast(false), 4000);
+      return;
+    }
+    if (selectedProduct && "discountPrice" in selectedProduct) {
+      await buyProduct("NY", selectedProduct.name, quantity, setUserInfo);
+
+      if (
+        tutorial.tutorialStep === 2 &&
+        selectedProduct.name === EProduct.HERB
+      ) {
+        tutorial.onTutorialProgress();
+        handleTutorialComplete();
+      }
+    }
+    setSelectedProduct(null);
+    setQuantity(1);
+    WebApp.HapticFeedback.impactOccurred("heavy");
+  };
+
+  const handleProductSelect = (product: Product | MarketProduct) => {
+    WebApp.HapticFeedback.impactOccurred("heavy");
+    if (tutorial.tutorialStep === 2 && product.name !== EProduct.HERB) {
+      return;
+    }
+    setSelectedProduct((prevSelectedProduct) =>
+      prevSelectedProduct?.name === product.name ? null : product,
+    );
+    setQuantity(tutorial.tutorialStep === 2 ? 5 : 1);
+  };
+
+  const handleUnlockClick = (product: MarketProduct | undefined) => {
+    if (product) {
+      onUnlockClick(tabMapping[product.name]);
+    } else {
+      onUnlockClick("shipping");
+    }
+    onClose();
+  };
+
+  const handleRedirectToTilkRoad = () => {
+    setActiveTab("Tilk Road");
+  };
+
+  const handleShip = (
+    shippingMethod: EShippingMethod,
+    product: EProduct,
+    amount: number,
+  ) => {
+    shipProduct(
+      "NY",
+      {
+        shippingMethod,
+        product,
+        amount,
+      },
+      setUserInfo,
+    ); // TODO
+  };
+
+  if (!isOpen) return null;
+  return (
+    <FixedOverlay onClick={onClose}>
+      <ModalContainer onClick={(e) => e.stopPropagation()}>
+        <Notch />
+        <TabContainer>
+          <Tab
+            active={activeTab === "Tilk Road"}
+            onClick={() => setActiveTab("Tilk Road")}
+          >
+            Tilk Road
+          </Tab>
+          <Tab
+            active={activeTab === "Tedex"}
+            onClick={() => setActiveTab("Tedex")}
+          >
+            Tedex
+          </Tab>
+        </TabContainer>
+
+        {activeTab === "Tilk Road" && (
+          <TilkRoadModal
+            remainingCash={remainingCash}
+            totalCost={totalCost}
+            marketInfo={marketInfo}
+            userInfo={userInfo}
+            selectedProduct={selectedProduct}
+            quantity={quantity}
+            handleBuy={handleBuy}
+            setQuantity={setQuantity}
+            handleProductSelect={handleProductSelect}
+            handleUnlockClick={handleUnlockClick}
+            tutorial={tutorial}
+          ></TilkRoadModal>
+        )}
+
+        {activeTab === "Tedex" && (
+          <TedexModal
+            userInfo={userInfo}
+            handleShip={handleShip}
+            shippingMethods={shippingMethods}
+            handleRedirectToTilkRoad={handleRedirectToTilkRoad}
+            handleUnlockClick={handleUnlockClick}
+          ></TedexModal>
+        )}
+
+        <CloseButton onClick={onClose}>&times;</CloseButton>
+        <RoundButton onClick={onClose}>&times;</RoundButton>
+        {showToast && (
+          <div className="fixed top-0 right-0 m-4 animate-slide-in-from-left animate-slide-out-to-right">
+            <div className="toast toast-top toast-end">
+              <div className="alert alert-error p-4 rounded shadow-lg text-white bg-red-600 font-bold">
+                <span>{error ? error : `Not enough cash.`}</span>
+              </div>
+            </div>
+          </div>
+        )}
+      </ModalContainer>
+    </FixedOverlay>
+  );
+};
---
+++ src/components/shop/RenderUpgrades.tsx
@@ -0,0 +1,229 @@
+import React from "react";
+import {
+  DealerUpgrade,
+  EDealerUpgrade,
+  EUpgradeCategory,
+  IRequirement,
+  IUpgrade,
+  ProductUpgrade,
+  RequirementType,
+} from "../interfaces/upgrade.interface";
+import { IUserInfo, Product } from "../interfaces/user.interface";
+import { EProduct } from "../interfaces/product.interface";
+import BuyCard from "../BuyCard";
+import { CardRequirement } from "../styled/cardStyled";
+import { TouchPoint } from "../utils/types";
+import { IMarketInfo } from "../interfaces/market.interface";
+
+interface RenderUpgradesProps {
+  userInfo: IUserInfo;
+  tab: string;
+  upgradesData: IUpgrade | undefined;
+  setTouchPoints: React.Dispatch<React.SetStateAction<TouchPoint[]>>;
+  setUserInfo: React.Dispatch<React.SetStateAction<IUserInfo>>;
+  setUpgrades: React.Dispatch<React.SetStateAction<IUpgrade | undefined>>;
+  setShowBalanceErrorToast: React.Dispatch<React.SetStateAction<boolean>>;
+  setMarketInfo: React.Dispatch<React.SetStateAction<IMarketInfo | undefined>>;
+  buyUpgrade: (
+    params: {
+      category: EUpgradeCategory;
+      upgrade: EProduct | EDealerUpgrade;
+    },
+    setUserInfo: React.Dispatch<React.SetStateAction<IUserInfo>>,
+    setMarketInfo: React.Dispatch<
+      React.SetStateAction<IMarketInfo | undefined>
+    >,
+  ) => Promise<void>;
+}
+
+export const RenderUpgrades: React.FC<RenderUpgradesProps> = ({
+  userInfo,
+  tab,
+  upgradesData,
+  setTouchPoints,
+  setUserInfo,
+  setShowBalanceErrorToast,
+  buyUpgrade,
+  setMarketInfo,
+}) => {
+  const render = (
+    upgrade: ProductUpgrade | DealerUpgrade,
+    key: EProduct | EDealerUpgrade,
+    category: EUpgradeCategory,
+    price: number,
+    level: number,
+    upgradeEffect?: string,
+    locked?: boolean,
+  ) => {
+    return (
+      <BuyCard
+        key={key}
+        item={{
+          image: upgrade.image,
+          title: upgrade.title,
+          cost: price,
+          level: level,
+          upgradeValue: upgradeEffect,
+          description: upgrade.description,
+          requirements: upgrade.requirements
+            ? {
+                name: upgrade.requirements[0].product,
+                level: upgrade.requirements[0].level,
+                requirement: upgrade.requirements[0].requirement,
+              }
+            : null,
+        }}
+        locked={locked || false}
+        onBuyClick={(params) => buyUpgrade(params, setUserInfo, setMarketInfo)}
+        upgradeOption={false}
+        userInfo={userInfo}
+        setUserInfo={setUserInfo}
+        setTouchPoints={setTouchPoints}
+        setShowBalanceErrorToast={setShowBalanceErrorToast}
+        category={category}
+        upgradeKey={key}
+        upgradeOptions={[]}
+      />
+    );
+  };
+
+  const renderUpgradeCategory = (
+    categoryTitle: string,
+    upgrades: Record<string, ProductUpgrade | DealerUpgrade> | undefined,
+    category: EUpgradeCategory,
+    userInfo: IUserInfo,
+  ) => (
+    <div key={categoryTitle}>
+      <h3 className="text-2xl font-semibold capitalize p-4">{categoryTitle}</h3>
+      <div className="space-y-2">
+        {Object.entries(upgrades ?? {}).map(([key, upgrade]) => {
+          const productUpgrade = upgrade as DealerUpgrade;
+          const userUpgrades = userInfo.dealerUpgrades;
+          const userUpgrade = userUpgrades.find((u) => u.upgrade === key);
+          const price: number =
+            userUpgrade?.upgradePrice || productUpgrade.basePrice;
+          const level = userUpgrade?.level || 0;
+          const upgradeRequirements =
+            productUpgrade.requirements as IRequirement[];
+          let locked = false;
+          if (upgradeRequirements) {
+            locked = upgradeRequirements.some((req) => {
+              const requiredProduct = userInfo.products.find(
+                (u) => u.name === req.product,
+              );
+              return !requiredProduct || requiredProduct.level < req.level;
+            });
+          }
+
+          const upgradeDiff =
+            userUpgrade?.upgradeAmount! - userUpgrade?.amount!;
+          let upgradeEffect: string | undefined;
+          switch (key) {
+            case EDealerUpgrade.SOCIAL_MEDIA_CAMPAGIN:
+              // case EDealerUpgrade.STREET_PROMOTION_TEAM:
+              // case EDealerUpgrade.CLUB_PARTNERSHIP:
+              upgradeEffect = `Adds ${
+                upgradeDiff || productUpgrade.amountMultiplier
+              } customers`;
+              break;
+            case EDealerUpgrade.COMBO_PACKS:
+            case EDealerUpgrade.HIGH_END_PACKAGING:
+            case EDealerUpgrade.PARTY_PACKS:
+            case EDealerUpgrade.HIGH_DOSE_PACKAGES:
+            case EDealerUpgrade.FESTIVAL_BLOTTERS:
+            case EDealerUpgrade.BULK_BAGS:
+              upgradeEffect = `Customers buy ${
+                upgradeDiff || productUpgrade.amountMultiplier
+              } more product`;
+              break;
+          }
+
+          return render(
+            upgrade,
+            key as EProduct | EDealerUpgrade,
+            category,
+            price,
+            level,
+            upgradeEffect,
+            locked,
+          );
+        })}
+      </div>
+    </div>
+  );
+
+  const renderProductCategory = (
+    categoryTitle: string,
+    upgrades: Record<string, ProductUpgrade | DealerUpgrade> | undefined,
+    category: EUpgradeCategory,
+    userInfo: IUserInfo,
+  ) => (
+    <div key={categoryTitle}>
+      <h3 className="text-2xl font-semibold capitalize p-4">{categoryTitle}</h3>
+      <div className="space-y-2">
+        {Object.entries(upgrades ?? {}).map(([key, upgrade]) => {
+          const productUpgrade = upgrade as ProductUpgrade;
+          const userUpgrades = userInfo.products;
+          const userUpgrade = userUpgrades.find((u: Product) => u.name === key);
+          const price = userUpgrade?.upgradePrice || productUpgrade.basePrice;
+          const level = userUpgrade?.level || 0;
+          const upgradeRequirements = productUpgrade.requirements;
+          let locked = false;
+          if (upgradeRequirements) {
+            locked = upgradeRequirements.some((req) => {
+              if (req.requirement == "product") {
+                const requiredProduct = userInfo.products.find(
+                  (u) => u.name === req.product,
+                );
+                return !requiredProduct || requiredProduct.level < req.level;
+              } else if (req.requirement == "referredUsers") {
+                return userInfo.referredUsers.length < req.level;
+              }
+            });
+          }
+          const upgradeEffect = userUpgrade?.upgradeMarketDiscount
+            ? `Discount from ${userUpgrade?.marketDiscount.toFixed(
+                2,
+              )}% to ${userUpgrade?.upgradeMarketDiscount.toFixed(2)}%`
+            : `Unlock a ${productUpgrade.baseDiscount}% discount`;
+          return render(
+            productUpgrade,
+            key as EProduct | EDealerUpgrade,
+            category,
+            price,
+            level,
+            upgradeEffect,
+            locked,
+          );
+        })}
+      </div>
+    </div>
+  );
+
+  if (!upgradesData) return <></>;
+
+  const renderAllDealerCategories = () => (
+    <>
+      {renderProductCategory(
+        "Products Discounts",
+        upgradesData?.product ?? {},
+        EUpgradeCategory.PRODUCT,
+        userInfo,
+      )}
+      {renderUpgradeCategory(
+        "Customers Boosters",
+        upgradesData?.dealer ?? {},
+        EUpgradeCategory.DEALER,
+        userInfo,
+      )}
+    </>
+  );
+
+  return (
+    <div className="space-y-4 scrollable-content">
+      {tab === "dealer" && renderAllDealerCategories()}
+    </div>
+  );
+};
+
+export default RenderUpgrades;
