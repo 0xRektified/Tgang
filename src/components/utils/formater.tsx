456d253784757a4148c4f162b1d8c33662ff68e6
XORs33r <bugislife@proton.me>
2024-09-17 15:28:04 +0800

    fix: formater to seconds

--- src/components/utils/formater.tsx
+++ src/components/utils/formater.tsx
@@ -10,7 +10,14 @@ export const formatPrice = (price: number, showDecimals: boolean) => {
 export const convertSecondsToReadableTime = (seconds: number): string => {
   const hours = Math.floor(seconds / 3600);
   const minutes = Math.floor((seconds % 3600) / 60);
-  return `${hours}h${minutes ? `${minutes}m` : ""}`;
+  const remainingSeconds = Math.floor(seconds % 60);
+
+  let result = '';
+  if (hours > 0) result += `${hours}h`;
+  if (minutes > 0) result += `${minutes}m`;
+  result += `${remainingSeconds}s`;
+
+  return result;
 };
 
 export const formatSeconds = (time: number) => {
