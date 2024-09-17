8fa86d5de4d06ea5a7bb3226488d70cfe5e4a4db
XORs33r <bugislife@proton.me>
2024-09-17 18:27:07 +0800

    feat: add favicon and manifest

--- src/main.tsx
+++ src/main.tsx
@@ -9,6 +9,12 @@ import { initializeApp } from "./appScreenHelper";
 // Initialize the screen logic to avoid the app to be closed
 initializeApp();
 // this manifest is used temporarily for development purposes
+
+
+
+// const manifestUrl =
+//   "https://app.cartel-game.com/manifest.json";
+
 const manifestUrl =
   "https://raw.githubusercontent.com/ton-community/tutorials/main/03-client/test/public/tonconnect-manifest.json";
 
