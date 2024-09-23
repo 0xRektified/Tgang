56e6c3e2a31374298c59cfb97d286ac74125ad6f
Alko89 <alefer89@gmail.com>
2024-09-23 23:36:28 +0200

    fix: float

---
+++ src/components/airdrop/FriendsComponent.tsx
@@ -0,0 +1,268 @@
+import React, { useEffect } from "react";
+import WebApp from "@twa-dev/sdk";
+import { IReferredUsers, IUserInfo } from "../interfaces/user.interface";
+import { useTutorial } from "../../hooks/useTutorial";
+
+import { PiCopySimpleBold } from "react-icons/pi";
+import {
+  Button,
+  Card,
+  Stats,
+  StatDesc,
+  StatValue,
+  Table,
+  TableContainer,
+} from "./styles/airdrop.css";
+import styled from "styled-components";
+import { NeonButton } from "../styled/cardStyled";
+import { SkipButton } from "../home/Home";
+
+const AirdropContainer = styled.div`
+  background: #171c24;
+  color: white;
+  padding: 0.3rem;
+  width: 100%;
+  font-family: "Digital", sans-serif;
+`;
+
+const DigitalFont = styled.span`
+  font-family: "Digital", sans-serif;
+  font-size: 0.9rem;
+`;
+
+const ScrollableTableContainer = styled(TableContainer)`
+  max-height: calc(100vh - 300px);
+  overflow-y: auto;
+  -webkit-overflow-scrolling: touch;
+  padding-bottom: 10rem;
+`;
+
+const AnnouncementContainer = styled.div`
+  background: #2c3e50;
+  color: #ecf0f1;
+  padding: 0.5rem;
+  margin-top: 0.5rem;
+  border-radius: 4px;
+  font-weight: bold;
+  text-align: center;
+`;
+
+const AnnouncementLink = styled.a`
+  color: #3498db;
+  text-decoration: underline;
+  &:hover {
+    color: #2980b9;
+  }
+`;
+
+const InlineStatDesc = styled(StatDesc)`
+  display: flex;
+  align-items: center;
+  gap: 0.5em;
+`;
+
+const FriendNeonButton = styled(NeonButton)`
+  width: 100%;
+  height: 3rem;
+`;
+
+const CopyNeonButton = styled(NeonButton)`
+  height: 3rem;
+  width: 3rem;
+  padding: 0;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+const TutorialOverlay = styled.div`
+  position: fixed;
+  top: 0;
+  left: 0;
+  right: 0;
+  bottom: 0;
+  background-color: rgba(0, 0, 0, 0.7);
+  z-index: 1000;
+  display: flex;
+  padding-top: 5em;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+`;
+
+const TutorialText = styled.div`
+  color: white;
+  font-size: 1.2rem;
+  text-align: center;
+  margin: 1rem 0;
+  max-width: 80%;
+`;
+
+interface FriendsComponentProps {
+  referralToken: string;
+  referredUsers: IReferredUsers[];
+  userInfo: IUserInfo;
+  tutorial: ReturnType<typeof useTutorial>;
+}
+
+const FriendsComponent: React.FC<FriendsComponentProps> = ({
+  referralToken,
+  referredUsers,
+  userInfo,
+  tutorial,
+}) => {
+  const handleRefForward = () => {
+    WebApp.openTelegramLink(
+      `https://t.me/share/url?url=${
+        import.meta.env.VITE_WEB_APP_URL
+      }?startapp=${referralToken}`,
+    );
+    if (tutorial.tutorialStep === 5) {
+      tutorial.setTutorialCompleted(true);
+      tutorial.tutorialCompleted = true;
+    }
+  };
+
+  const handleRefClick = () => {
+    navigator.clipboard.writeText(
+      `${import.meta.env.VITE_WEB_APP_URL}?startapp=${referralToken}`,
+    );
+  };
+
+  useEffect(() => {
+    if (tutorial.tutorialStep === 5) {
+      const inviteFriendsButton = document.querySelector(
+        ".invite-friends-button",
+      );
+      if (inviteFriendsButton) {
+        inviteFriendsButton.classList.add("tutorial-highlight");
+        tutorial.onTutorialProgress();
+      }
+    }
+  }, [tutorial.tutorialStep]);
+
+  const handleSkipTutorial = () => {
+    tutorial.setTutorialCompleted(true);
+    tutorial.tutorialCompleted = true;
+  };
+
+  return (
+    <div className="space-y-4 scrollable-content">
+      <AirdropContainer>
+        {!tutorial.tutorialCompleted && tutorial.tutorialStep === 5 ? (
+          <TutorialOverlay>
+            <TutorialText>
+              "Invite Friends" below to win real-world money
+            </TutorialText>
+            <FriendNeonButton onClick={handleRefForward}>
+              Invite Friends
+            </FriendNeonButton>
+            <AnnouncementContainer>
+              <p>
+                🎉 Exciting news! Beta testers can win real-world prizes
+                totaling over $600 USD!
+              </p>
+              <AnnouncementLink
+                href="https://t.me/cartel_game_community"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                Details in our community page
+              </AnnouncementLink>{" "}
+              🚀
+            </AnnouncementContainer>
+
+            <SkipButton onClick={handleSkipTutorial}>
+              End of the Tutorial
+            </SkipButton>
+          </TutorialOverlay>
+        ) : (
+          <>
+            <AnnouncementContainer>
+              <p>
+                🎉 Exciting news! Beta testers can win real-world prizes
+                totaling over $600 USD!
+              </p>
+              <AnnouncementLink
+                href="https://t.me/cartel_game_community"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                Details in our community page
+              </AnnouncementLink>{" "}
+              🚀
+            </AnnouncementContainer>
+          </>
+        )}
+      </AirdropContainer>
+      <Card className="scrollable-content">
+        <div className="flex flex-row items-center justify-start mb-4">
+          <Stats>
+            <InlineStatDesc>
+              Invited users:
+              <StatValue>{referredUsers.length}</StatValue>
+            </InlineStatDesc>
+          </Stats>
+        </div>
+        <div className="flex w-full mb-4">
+          <div className="flex-grow">
+            {!tutorial.tutorialCompleted &&
+            tutorial.tutorialStep === 5 ? null : (
+              <FriendNeonButton onClick={handleRefForward}>
+                Invite Friends
+              </FriendNeonButton>
+            )}
+          </div>
+          <div className="ml-4">
+            <CopyNeonButton onClick={handleRefClick}>
+              <PiCopySimpleBold className="text-xl font-bold" />
+            </CopyNeonButton>
+          </div>
+        </div>
+
+        <div className="grid grid-cols-2 gap-4 w-full pb-4">
+          <div className="card flex flex-col justify-center items-center text-center">
+            <h3 className="font-bold">
+              Per invite <span className="text-2xl">💰</span>
+            </h3>
+            <p>$1000 & 100rep</p>
+          </div>
+          <div className="card flex flex-col justify-center items-center text-center">
+            <h3 className="font-bold">
+              Premium <span className="text-2xl">💰💰</span>
+            </h3>
+            <p>$2000 & 1000rep</p>
+          </div>
+        </div>
+        <ScrollableTableContainer>
+          <Table>
+            <thead>
+              <tr>
+                <th>#</th>
+                <th>Referred Users</th>
+                <th></th>
+              </tr>
+            </thead>
+            <tbody>
+              {[...Array(Math.max(3, referredUsers.length))].map((_, index) => (
+                <tr key={index}>
+                  <td>{index + 1}</td>
+                  <td>{referredUsers[index]?.username || "-"}</td>
+                  <td>
+                    {referredUsers[index]?.reward && (
+                      <span style={{ color: "#32cd32" }}>
+                        +{referredUsers[index]?.reward}$
+                      </span>
+                    )}
+                  </td>
+                </tr>
+              ))}
+            </tbody>
+          </Table>
+        </ScrollableTableContainer>
+      </Card>
+    </div>
+  );
+};
+
+export default FriendsComponent;
---
+++ src/components/home/ShippingCard.tsx
@@ -0,0 +1,328 @@
+import React, { useEffect, useState } from "react";
+import styled from "styled-components";
+import { CardContainer } from "./styles/shipping.css";
+import { IShippingMethod } from "../interfaces/shipping.interface";
+import { IUserShipping } from "../interfaces/user.interface";
+import { Duration } from "date-fns";
+import { convertSecondsToReadableTime } from "../utils/formater";
+
+const ShippingCardMiddle = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  flex: 1;
+  margin: 0 1rem;
+`;
+
+const ShippingCardRight = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: flex-end;
+`;
+
+const NeonButton = styled.button`
+  background-color: rgb(39 39 42) !important;
+  color: #e4e4e7;
+  border-radius: 8px;
+  padding: 0.3rem 0.6rem;
+  border: 2px solid #1e90ff;
+  cursor: pointer;
+  font-weight: bold;
+  font-size: 0.8em;
+  text-transform: uppercase;
+  letter-spacing: 0.03em;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  text-align: center;
+  gap: 0.3rem;
+  transition: background-color 0.3s ease, transform 0.1s ease;
+  box-shadow: 0 0 2px #1e90ff, 0 0 4px #1e90ff, 0 0 6px #1e90ff, 0 0 8px #1e90ff;
+  max-width: 100px;
+  white-space: nowrap;
+  overflow: hidden;
+  text-overflow: ellipsis;
+
+  &:hover {
+    background-color: rgb(24 24 27);
+    animation: glow 1.5s infinite alternate, pulse 2s infinite;
+  }
+
+  &:active {
+    animation: glow 1.5s infinite alternate, pulse 2s infinite;
+  }
+
+  &:disabled {
+    background-color: rgb(99 99 99) !important;
+    border: none;
+    cursor: not-allowed;
+  }
+
+  @keyframes glow {
+    0% {
+      box-shadow: 0 0 2px #1e90ff, 0 0 4px #1e90ff, 0 0 6px #1e90ff,
+        0 0 8px #1e90ff;
+    }
+    100% {
+      box-shadow: 0 0 8px #1e90ff, 0 0 12px #1e90ff, 0 0 16px #1e90ff,
+        0 0 20px #1e90ff;
+    }
+  }
+
+  @keyframes pulse {
+    0% {
+      transform: scale(1);
+    }
+    50% {
+      transform: scale(1.05);
+    }
+    100% {
+      transform: scale(1);
+    }
+  }
+`;
+
+const ShipNowText = styled.div`
+  color: white;
+  font-weight: bold;
+  font-size: 1.1em;
+  text-shadow: 0 0 5px #1e90ff, 0 0 10px #1e90ff;
+  animation: pulse 1.5s infinite;
+
+  @keyframes pulse {
+    0% {
+      opacity: 1;
+    }
+    50% {
+      opacity: 0.5;
+    }
+    100% {
+      opacity: 1;
+    }
+  }
+`;
+
+const ShippingCardContainer = styled(CardContainer)<{ locked: boolean }>`
+  background: linear-gradient(135deg, #282c34, #3c3f45);
+  border-radius: 0.5rem;
+  box-shadow: 0 4px 6px rgba(0, 0, 0, 0.1);
+  overflow: hidden;
+  position: relative;
+  border: 2px solid ${(props) => (props.locked ? "#4a4a4a" : "#285d90")};
+  display: flex;
+  gap: 0.5rem;
+  transition: all 0.3s ease;
+
+  &:hover {
+    box-shadow: ${(props) =>
+      props.locked ? "none" : "0 0 8px #1e90ff, 0 0 12px #1e90ff"};
+  }
+`;
+
+const ShippingCardImage = styled.img`
+  border-radius: 0.8rem;
+  width: 50px;
+  height: 50px;
+`;
+
+const ShippingCardTitle = styled.h4`
+  font-size: 1rem;
+  font-weight: bold;
+  font-size: 1rem;
+  margin-bottom: 0.5rem;
+`;
+
+const ShippingCardContent = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  width: 100%;
+  padding: 0.5rem;
+`;
+
+const ShippingCardLeft = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`;
+
+const InfoText = styled.div`
+  font-size: 0.8rem;
+  color: #a0aec0;
+  margin: 0;
+  color: white;
+`;
+
+const PriceText = styled.span`
+  color: #4ade80;
+`;
+
+const WhiteText = styled.span`
+  color: white;
+`;
+
+const LoadingSpinner = styled.div`
+  border: 4px solid rgba(255, 255, 255, 0.3);
+  border-radius: 50%;
+  border-top: 4px solid #ffffff;
+  width: 24px;
+  height: 24px;
+  animation: spin 1s linear infinite;
+
+  @keyframes spin {
+    0% {
+      transform: rotate(0deg);
+    }
+    100% {
+      transform: rotate(360deg);
+    }
+  }
+`;
+
+export const ShippingCard: React.FC<{
+  method: IShippingMethod;
+  userShipping: IUserShipping | undefined;
+  locked: boolean;
+  handleUnlockClick: () => void;
+  handleOpenModal: (userShipping: IUserShipping) => void;
+}> = ({ method, userShipping, locked, handleUnlockClick, handleOpenModal }) => {
+  const [countdown, setCountdown] = useState<Duration>({
+    hours: 0,
+    minutes: 0,
+    seconds: 0,
+  });
+  const [loading, setLoading] = useState(true);
+
+  const shipmentInProgress = (nextShipment: Date) => {
+    return nextShipment.getTime() > new Date().getTime();
+  };
+
+  const calculateCountdown = (nextShipment: Date) => {
+    const now = new Date().getTime();
+    const nextShipmentTime = nextShipment.getTime();
+    if (nextShipmentTime < now) {
+      return {
+        hours: 0,
+        minutes: 0,
+        seconds: 0,
+      };
+    }
+    const duration = {
+      hours: Math.floor(
+        (nextShipment.getTime() - new Date().getTime()) / (1000 * 60 * 60),
+      ),
+      minutes:
+        Math.floor(
+          (nextShipment.getTime() - new Date().getTime()) / (1000 * 60),
+        ) % 60,
+      seconds:
+        Math.floor((nextShipment.getTime() - new Date().getTime()) / 1000) % 60,
+    };
+    return duration;
+  };
+
+  const formatCountdown = (countdown: Duration): string => {
+    const { hours, minutes, seconds } = countdown;
+
+    if (hours === 0 && minutes === 0 && seconds === 0) {
+      return "Ship now";
+    }
+
+    const parts = [];
+    if (hours && hours > 0) parts.push(`${hours}h`);
+    if (minutes && minutes > 0) parts.push(`${minutes}m`);
+    if (seconds && seconds > 0) parts.push(`${seconds}s`);
+
+    return parts.join(" ");
+  };
+
+  const renderLockedInfo = (method: IShippingMethod) => (
+    <ShippingCardMiddle>
+      <ShippingCardMiddle>
+        <InfoText>
+          Price <PriceText>${method.basePrice}</PriceText>
+        </InfoText>
+        <InfoText>Capacity {method.baseCapacity}</InfoText>
+        <InfoText>
+          Shipping {convertSecondsToReadableTime(method.baseShippingTime)}
+        </InfoText>
+      </ShippingCardMiddle>
+    </ShippingCardMiddle>
+  );
+
+  useEffect(() => {
+    if (userShipping && !locked) {
+      setLoading(true);
+      const timer = setTimeout(() => {
+        setLoading(false);
+        const newCountdown = calculateCountdown(
+          new Date(userShipping.nextShipment),
+        );
+        setCountdown(newCountdown);
+      }, 1000);
+
+      const interval = setInterval(() => {
+        const newCountdown = calculateCountdown(
+          new Date(userShipping.nextShipment),
+        );
+        setCountdown(newCountdown);
+      }, 1000);
+
+      return () => {
+        clearTimeout(timer);
+        clearInterval(interval);
+      };
+    }
+  }, [userShipping, locked]);
+
+  const renderShippingButton = () => {
+    if (locked) {
+      return <NeonButton onClick={handleUnlockClick}>Unlock</NeonButton>;
+    } else if (
+      userShipping &&
+      shipmentInProgress(new Date(userShipping.nextShipment))
+    ) {
+      return <NeonButton disabled>In Progress</NeonButton>;
+    } else if (userShipping) {
+      return (
+        <NeonButton onClick={() => handleOpenModal(userShipping)}>
+          Ship {userShipping.capacity}
+        </NeonButton>
+      );
+    }
+    return null;
+  };
+
+  return (
+    <ShippingCardContainer locked={locked}>
+      <ShippingCardContent>
+        <ShippingCardLeft>
+          <ShippingCardTitle>{method.title}</ShippingCardTitle>
+          <ShippingCardImage src={method.image} alt={method.title} />
+        </ShippingCardLeft>
+        {locked ? (
+          renderLockedInfo(method)
+        ) : (
+          <ShippingCardMiddle>
+            {userShipping && (
+              <div>
+                {loading ? (
+                  <LoadingSpinner />
+                ) : formatCountdown(countdown) === "Ship now" ? (
+                  <ShipNowText>Next shipment Ready</ShipNowText>
+                ) : (
+                  <div>
+                    <div>Next shipment</div>
+                    <div>{formatCountdown(countdown)}</div>
+                  </div>
+                )}
+              </div>
+            )}
+          </ShippingCardMiddle>
+        )}
+        <ShippingCardRight>{renderShippingButton()}</ShippingCardRight>
+      </ShippingCardContent>
+    </ShippingCardContainer>
+  );
+};
