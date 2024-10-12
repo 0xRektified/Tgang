569df034ec06b3957367fdaec891885a84e75066
XORs33r <bugislife@proton.me>
2024-10-12 12:12:55 +0800

    feat: update timer style

---
+++ src/components/interfaces/user.interface.ts
@@ -0,0 +1,145 @@
+import { EProduct } from "./product.interface";
+import { EShippingMethod } from "./shipping.interface";
+import { SocialChannel } from "./social.interface";
+import { EDealerUpgrade, IRequirement, IUpgrade } from "./upgrade.interface";
+
+export interface UserLab {
+  product: EProduct;
+  title: string;
+  image: string;
+  capacityLevel: number;
+  productionLevel: number;
+  capacity: number;
+  production: number;
+  upgradeCapacityPrice: number;
+  upgradeCapacity: number;
+  lastCapacityUpgrade: Date;
+  nextCapacityUpgrade: Date;
+  upgradeProductionPrice: number;
+  upgradeProduction: number;
+  lastProductionUpgrade: Date;
+  nextProductionUpgrade: Date;
+  collectTime: Date;
+  produced: number;
+}
+
+export interface LabPlot {
+  plotId: number;
+  lab?: UserLab;
+}
+
+export interface IUserShipping {
+  method: EShippingMethod;
+  title: string;
+  image: string;
+  capacityLevel: number;
+  shippingTimeLevel: number;
+  capacity: number;
+  shippingTime: number;
+  upgradeCapacityPrice: number;
+  upgradeCapacity: number;
+  lastCapacityUpgrade: Date;
+  nextCapacityUpgrade: Date;
+  upgradeShippingTimePrice: number;
+  upgradeShippingTime: number;
+  lastShippingTimeUpgrade: Date;
+  nextShippingTimeUpgrade: Date;
+  lastShipment: Date;
+  nextShipment: Date;
+  requirements: IRequirement[] | null;
+}
+
+export interface Product {
+  name: EProduct;
+  quantity: number;
+  image: string;
+  level: number;
+  upgradePrice: number;
+  lastUpgrade: Date;
+  nextUpgrade: Date;
+  marketDiscount: number;
+  upgradeMarketDiscount: number;
+  selected: boolean;
+  slot: number | null;
+}
+
+export interface UserDealerUpgrade {
+  upgrade: EDealerUpgrade;
+  product: EProduct | null;
+  title: string;
+  image: number;
+  level: number;
+  amount: number;
+  upgradePrice: number;
+  lastUpgrade: Date;
+  nextUpgrade: Date;
+  upgradeAmount: number;
+  requirements: IRequirement[] | null;
+}
+
+export interface IReputationLevel {
+  level: number;
+  minReputation: number;
+  maxReputation: number;
+  title: string;
+}
+
+export interface IReferredUsers {
+  id: string;
+  username: string;
+  reward: number;
+}
+
+export interface IUserSocial {
+  channel: SocialChannel;
+  member: boolean;
+  joined?: Date;
+}
+
+export interface IUserAchievements {
+  [key: number]: boolean;
+}
+
+export interface IUserInfo {
+  id: string;
+  username: string;
+  cashAmount: number;
+  upgrades: IUpgrade[];
+  products: Product[];
+  dealerUpgrades: UserDealerUpgrade[];
+  shipping: IUserShipping[];
+  labPlots: LabPlot[];
+  labPlotPrice: number;
+  referralToken: string;
+  referredUsers: IReferredUsers[];
+  customerAmount: number;
+  customerAmountMax: number;
+  lastSell: Date;
+  customerAmountRemaining: number;
+  robberyStrike: number;
+  lastRobbery?: Date;
+  lastShipment?: Date;
+  nextShipment: Date;
+  reputation: number;
+  userLevel: IReputationLevel;
+  socials?: IUserSocial[];
+  wallet?: string;
+  pvp?: IUserPvp;
+  achievements?: IUserAchievements;
+}
+
+export interface IUserPvp {
+  victory: number;
+  defeat: number;
+  lastAttackDate: Date;
+  attacksToday: number;
+  lastDefendDate: Date;
+  attacksAvailable: number;
+  healthPoints: number;
+  protection: number;
+  damage: number;
+  accuracy: number;
+  evasion: number;
+  lootPower: number;
+  criticalChance: number;
+}
---
+++ src/hooks/useAuthAndFetchUserData.ts
@@ -0,0 +1,81 @@
+import WebApp from "@twa-dev/sdk";
+import queryString from "query-string";
+import validator from "validator";
+import { useState, useEffect } from "react";
+import axios from "axios";
+import { IUserInfo } from "../components/interfaces/user.interface";
+import axiosInstance, { setAuthToken } from "../api/axiosConfig";
+import mixpanel from "mixpanel-browser";
+
+export function useAuthAndFetchUserData(
+  setUser: React.Dispatch<React.SetStateAction<IUserInfo>>,
+) {
+  const [loading, setLoading] = useState<boolean>(true);
+  const [error, setError] = useState<string | null>(null);
+  const [signup, setSignup] = useState<boolean>(false);
+
+  useEffect(() => {
+    const login = async () => {
+      let sanitizedResult: Record<string, any> = {};
+      try {
+        const decodedInput = decodeURIComponent(WebApp.initData);
+        const parsedQuery = queryString.parse(decodedInput);
+        const sanitizeQuery = (query: Record<string, any>) => {
+          const sanitizedQuery: Record<string, any> = {};
+          if (query.query_id && typeof query.query_id === "string") {
+            sanitizedQuery.query_id = validator.escape(query.query_id);
+          }
+          if (query.auth_date && typeof query.auth_date === "string") {
+            sanitizedQuery.auth_date = validator.escape(query.auth_date);
+          }
+          if (query.user && typeof query.user === "string") {
+            try {
+              const user = JSON.parse(query.user);
+              if (user.id && validator.isInt(user.id.toString())) {
+                sanitizedQuery.user = {
+                  id: user.id,
+                  first_name: validator.escape(user.first_name),
+                  username: user.username
+                    ? validator.escape(user.username)
+                    : undefined,
+                  language_code: validator.escape(user.language_code),
+                };
+              }
+            } catch (error) {
+              throw new Error(`Invalid user JSON format ${error}`);
+            }
+          }
+          return sanitizedQuery;
+        };
+        const sanitizedResult = sanitizeQuery(parsedQuery);
+        const response = await axios.post<{
+          access_token: string;
+          signup: boolean;
+        }>(
+          `${import.meta.env.VITE_BACKEND_URL}/auth/login?${decodedInput}`,
+          sanitizedResult,
+        );
+        const { access_token, signup } = response.data;
+        setSignup(signup);
+        setAuthToken(access_token);
+        const userInfoResponse = await axiosInstance.get<IUserInfo>(`/users`);
+        setUser(userInfoResponse.data);
+
+        mixpanel.identify(userInfoResponse.data.id.toString());
+        mixpanel.people.set({
+          $name: userInfoResponse.data.username,
+          $last_login: new Date(),
+        });
+        mixpanel.track("Login");
+      } catch (error) {
+        console.error("Failed to parse and sanitize query or login:", error);
+        setError(`Failed to authenticate and fetch user data ${error}`);
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    login();
+  }, [setLoading, setError]);
+  return { loading, error, signup };
+}
