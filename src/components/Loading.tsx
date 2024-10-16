472703cfc53a601ba86a6162b48f33c2b1005215
XORs33r <bugislife@proton.me>
2024-10-16 10:56:43 +0800

    feat: update loading messages and stuff

--- src/components/Loading.tsx
+++ src/components/Loading.tsx
@@ -171,7 +171,7 @@ const Loading: React.FC<LoadingProps> = ({ isContentLoaded }) => {
         <ContentWrapper>
           <NewsContainer>
             <NewsContent>
-              Checkout the new Crafting section and boost your Pvp powers !
+              Checkout the new LAB Crafting section and boost your Pvp powers !
             </NewsContent>
           </NewsContainer>
           <LoadingSpinner className="loading loading-spinner loading-md" />
